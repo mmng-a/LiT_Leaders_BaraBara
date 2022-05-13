77eacaa468977c9b3cba95abeb6dfbbaa33bee9e
Masashi Aso <masashi.apple.japan@icloud.com>
2022-05-13 15:28:18 +0900

    done

---
+++ BaraBara/RankingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RankingViewController.swift
+//  BaraBara
+//
+//  Created by Masashi Aso on 2022/05/13.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+  
+  @IBOutlet var rankingLabel1: UILabel!
+  @IBOutlet var rankingLabel2: UILabel!
+  @IBOutlet var rankingLabel3: UILabel!
+  let defaults: UserDefaults = UserDefaults.standard
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+    rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+    rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+  }
+  
+  @IBAction func toTop() {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+}
